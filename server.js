f6c2a4103e971302c6e6945d2c0a04cbe9bc29db
miraj <miraj.ansari@sysfore.com>
2023-01-16 18:42:24 +0530

    (feat): user controller added

--- server.js
+++ server.js
@@ -11,7 +11,18 @@ const config = require('./config/init.js');
 
 config.init();
 
+app.get('/users', UserController.index);
+app.post('/users', UserController.create);
+app.get('/users/:id', UserController.read);
+app.put('/users/:id', UserController.update);
+app.delete('/users/:id', UserController.delete);
+app.post('/users/login', UserController.login);
+
 
 const appRoutes = require('./routes/App.routes');
 app.use('/', appRoutes);
+
+app.listen(3000, function() {
+    console.log('Server started on port 3000');
+});
 
