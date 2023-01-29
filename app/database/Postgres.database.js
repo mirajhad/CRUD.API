afa82483599a53a63a04db3b2d17fd98613f0e7a
miraj <miraj.ansari@sysfore.com>
2023-01-29 21:25:02 +0530

    (feat): database added

--- app/database/Postgres.database.js
+++ app/database/Postgres.database.js
@@ -1,13 +1,14 @@
-
 const { Client } = require('pg');
 const dbConfig = require('../config/db.conf');
 
 const client = new Client({
-    user: dbConfig.user,
-    host: dbConfig.host,
-    database: dbConfig.name,
-    password: dbConfig.password,
-    port: dbConfig.port,
+  user: dbConfig.user,
+  host: dbConfig.host,
+  database: dbConfig.database,
+  password: dbConfig.password,
+  port: 5432,
 });
 
-client.connect();
+client.connect()
+  .then(() => console.log('Connected to PostgreSQL database'))
+  .catch(err => console.error('Error connecting to database', err));
